c986ff5336f3c75029dd6d154a756e748ab9bef0
Ouyei <36935397+Ouyei@users.noreply.github.com>
2022-01-11 09:01:28 -0800

    monty.c

---
+++ monty.c
@@ -0,0 +1,9 @@
+#include "monty.h"
+
+/**
+ * main - Entry 2 the program.
+ * @argc: Number of command line arguments.
+ * @argv: An array containing the arg.
+ * Return: Allways zero.
+ */
+int main(int argc, char **argv)
