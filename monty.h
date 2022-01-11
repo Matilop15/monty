a7f1abe8d1f48e1035336bae13472ed7c22956f9
Matias <3959@holbertonschool.com>
2022-01-11 10:20:31 -0800

    add functions in monty.h and file struct

--- monty.h
+++ monty.h
@@ -40,5 +40,7 @@ typedef struct instruction_s
 } instruction_t;
 
 int main(int argc, char **argv);
+void pall(stack_t **stack, unsigned int line_number);
+void push(stack_t **stack, unsigned int line_number);
 
 #endif
--- struct.c
+++ struct.c
@@ -1,1 +1,17 @@
-sdasdsa
+#include "monty.h"
+/**
+ *
+ *
+ */
+void busq_func(char *str)
+{
+	int i;
+
+	instruction_t options[] = {
+		{"push", push},
+		{"pall", pall}
+
+	};
+
+
+}
