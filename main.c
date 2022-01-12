2f9fcf3621793bd30d4d3241770e4cb4fc042090
Matias <3959@holbertonschool.com>
2022-01-12 04:58:54 -0800

    change name

---
+++ main.c
@@ -0,0 +1,18 @@
+#include "monty.h"
+
+/**
+ * main - Entry 2 the program.
+ * @argc: Number of command line arguments.
+ * @argv: An array containing the arg.
+ * Return: Allways zero.
+ */
+int main(int argc, char **argv)
+{
+	stack_t *stack
+
+	if (argc != 2)
+	{
+		printf("USAGE: monty file\n");
+		exit_type(&string);
+	}
+}
