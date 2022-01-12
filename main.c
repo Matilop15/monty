e7ad1cb54ed8b034bbee9a20df3d22fb719c8da5
Ouyei <36935397+Ouyei@users.noreply.github.com>
2022-01-12 06:37:34 -0800

    fixed_main

--- main.c
+++ main.c
@@ -8,29 +8,14 @@
  */
 int main(int argc, char **argv)
 {
-<<<<<<< HEAD
-	stack_t *stack
-=======
-	FILE *fo = NULL;
->>>>>>> e3c0846687f4e58af34c988ce9c02198cce5e850
+	stack_t *stack = NULL;
 
 	if (argc != 2)
 	{
 		printf("USAGE: monty file\n");
-<<<<<<< HEAD
-		exit_type(&string);
+		exit_type(&stack);
 	}
-=======
-		exit(EXIT_FAILURE);
-	}
-	fo = fopen(argv[1], "r"); /** r para abrir y leer */
-	if (fo == NULL)
-	{
-		dprintf(STDOUT_FILENO, "Error: Can't open file %s\n", argv[1]);
-		exit(EXIT_FAILURE);
-	}
-	else
-		llamar a struct 
-
->>>>>>> e3c0846687f4e58af34c988ce9c02198cce5e850
+	lec_file(av[1], &stack);
+	free_dlistint(stack);
+	return (0);
 }
