88a110f7395fa6bd86fa23af4cb639902ce421ac
Ouyei <36935397+Ouyei@users.noreply.github.com>
2022-01-12 09:34:48 -0800

    errores, monty update

--- monty.h
+++ monty.h
@@ -7,6 +7,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <stdarg.h>
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
@@ -48,9 +49,9 @@ char *parse_line(char *line);
 instruct_func get_op_func(char *str);
 stack_t *add_dnodeint_end(stack_t **head, const int n);
 stack_t *add_dnodeint(stack_t **head, const int n);
-void free_dlistint(stack_t *head);
+void free_dlistint(void);
 int delete_dnodeint_at_index(stack_t **head, unsigned int index);
-void error_salida(unsigned int error_code, ...);
 int isnumber(char *str);
+void error_salida(unsigned int error_code, ...);
 
 #endif
